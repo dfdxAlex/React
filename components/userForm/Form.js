84bae9449704a1eb7ad47a71750373d9b0adfb70
oleksandr-ishchuk <alexmway@mail.ru>
2023-09-17 23:51:10 +0200

    add style for  modul Form

--- components/userForm/Form.js
+++ components/userForm/Form.js
@@ -3,17 +3,61 @@ import "./Form.css";
 function Form()
 {
     return (
-        <div>
-            <form>
-                <label for="name">Имя актива</label>
-                <input id="name" type="text" ></input>
-                <label for="num">Число акций</label>
-                <input id="num" type="number" ></input>
-                <label for="price">Цена одной акции</label>
-                <input id="price" type="number" ></input>
-                <label for="price-close">Цена закрытия</label>
-                <input id="price-close" type="number" ></input>
-                <button>Добавить</button>
+        <div className="user-form">
+            <form className="user-form--form">
+                <label 
+                  className="user-form--label" 
+                  for="name"
+                >
+                  Имя актива
+                </label>
+                <input 
+                  className="user-form--input" 
+                  id="name" 
+                  type="text" 
+                >
+                </input>
+                <label 
+                  className="user-form--label" 
+                  for="num"
+                >
+                  Число акций
+                </label>
+                <input 
+                  className="user-form--input" 
+                  id="num" 
+                  type="number" 
+                >
+                </input>
+                <label 
+                  className="user-form--label" 
+                  for="price"
+                >
+                  Цена одной акции
+                </label>
+                <input 
+                  className="user-form--input" 
+                  id="price" 
+                  type="number" 
+                >
+                </input>
+                <label 
+                  className="user-form--label" 
+                  for="price-close"
+                >
+                  Цена закрытия
+                </label>
+                <input 
+                  className="user-form--input" 
+                  id="price-close" 
+                  type="number" 
+                >
+                </input>
+                <button
+                  className="user-form--button"
+                >
+                Добавить
+                </button>
             </form>
         </div>
     );
