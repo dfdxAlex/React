3f52ca4ff3608ca1647b73ef7b7cd419a04f05d6
oleksandr-ishchuk <alexmway@mail.ru>
2023-10-09 20:41:08 +0200

    add mas[obj]

--- App.js
+++ App.js
@@ -4,15 +4,43 @@ import Form from "./components/userForm/Form.js"
 
 function App() {
 
-  function getFormSelect(getSelectInForm)
-  {
-    console.log(getSelectInForm);
-  }
 
   function getObjUp(obj)
   {
     console.log(obj);
   }
+
+  function getFormSelect(getSelectInForm)
+  {
+    console.log(getSelectInForm);
+  }
+
+  let mas = [
+    {
+      nameA:"Google",
+      number:10,
+      price:200,
+      priceClose:190
+    },
+    {
+      nameA:"Apple",
+      number:20,
+      price:150,
+      priceClose:190
+    },
+    {
+      nameA:"Tesla",
+      number:4,
+      price:900,
+      priceClose:990
+    },
+    {
+      nameA:"Приват",
+      number:1,
+      price:230,
+      priceClose:240
+    }
+  ];
 
   return (
     <Color>
@@ -21,34 +49,19 @@ function App() {
         getObj={getObjUp}
       >
       </Form>
-      <Portfel  
-        name={"Google"}
-        number={10}
-        price={200}
-        priceClose={190}
-      >
-      </Portfel>
 
-      <Portfel 
-        name={"Apple"}
-        number={20}
-        price={150}
-        priceClose={190}
-      />
+      {
+          mas.map((el, index)=>(
+            <Portfel  
+              key={index}
+              name={el.nameA}
+              number={el.number}
+              price={el.price}
+              priceClose={el.priceClose}
+            />
+          ))
+      }
 
-      <Portfel 
-        name={"Tesla"}
-        number={4}
-        price={900}
-        priceClose={990}
-      />
-
-      <Portfel 
-        name={"Приват"}
-        number={1}
-        price={230}
-        priceClose={240}
-      />
   </Color>
   );
 }
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 // import AppApatch from './AppApatch';
-import SettingForCv from './SettingForCv';
+// import SettingForCv from './SettingForCv';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    // <App />
+    <App />
     // <AppApatch />
-    <SettingForCv />
+    // <SettingForCv />
 );
 
 
